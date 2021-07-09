a51c2b17f70d1ffbe57d6b07bd1668d9de4f4960
dexter-dz <dineshzaladekar@yahoo.in>
2021-07-09 17:58:56 +0530

    update 0.2
    Code optimization and trying to fix auto clear of $DISPLAY variable

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ echo "welcome $USER"
 echo "Checking the assigned Octate"
 cat /etc/resolv.conf | grep nameserver
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0
-
-echo "The new Display is set to $DISPLAY"
 echo "echo testing Display..."
 echo "Launching xcalc...... "
 xcalc
 echo "Hey $USER if Calculator didn't launch check whether you launched Xming with -ac"
 echo "If The Calculator launched successfully you are all set to launch GUI Apps"
+echo Type this command to set your Display
+echo "export DISPLAY=$ $DISPLAY"
