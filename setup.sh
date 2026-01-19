db683034c4dafbd0671872f4e386ee44fa1a5ba9
Dinesh Zaldekar <dineshzaladekar@yahoo.in>
2026-01-19 11:35:27 +0530

    Update setup.sh
    updated echo text.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Checking the assigned Octate"
 cat /etc/resolv.conf | grep nameserver
 export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0
 echo "echo testing Display..."
-echo "Launching xcalc...... "
+echo "Testing by Launching xcalc...... "
 xcalc
 echo "Hey $USER if Calculator didn't launch check whether you launched Xming with -ac"
 echo "If The Calculator launched successfully you are all set to launch GUI Apps"
